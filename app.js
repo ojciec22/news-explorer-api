782354089285b85a736f5f0095acc815b941ec4a
Prozaek <piterwave@gmail.com>
2021-01-21 00:15:00 +0300

    fix idn

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 
-// const limiter = require('./middlewares/limiter');
+const limiter = require('./middlewares/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { serverDown } = require('./constants/constants');
 
@@ -15,7 +15,7 @@ const routes = require('./routes');
 const errorHandler = require('./middlewares/error-handler');
 const { DB } = require('./config');
 
-const { PORT = 3000, NODE_ENV, DATABASE_URL } = process.env;
+const { PORT, NODE_ENV, DATABASE_URL } = process.env;
 const app = express();
 
 // подключаемся к серверу mongo
@@ -34,7 +34,7 @@ app.get('/crash-test', () => {
 });
 
 app.use(requestLogger); // ставить в начало цепочки
-// app.use(limiter); // отсекает запросы в определенный интервал времени
+app.use(limiter); // отсекает запросы в определенный интервал времени
 app.use(helmet()); // для установки заголовков безопасности
 
 app.use(bodyParser.json());
